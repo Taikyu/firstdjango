32cca5bf5abb93bf889585da4b51c7809e090381
unknown <endster5@gmail.com>
2022-10-29 00:13:16 +0100

    updated musicapp - added models to the app

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Artiste, Song, Lyric
 # Register your models here.
+
+
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.IntegerField()
+
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=4000)
+    song_id = models.IntegerField()
